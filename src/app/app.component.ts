22a1c1f595c058670a0e111fbbdc267ad9b4126f
Aadil Masood <aadilmasood@Aadils-MacBook-Air.local>
2022-06-19 15:54:58 -0500

    Added Angular Flex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'aadil-angular-blog';
+  title = 'Mohammad Aadil Masood';
 }
